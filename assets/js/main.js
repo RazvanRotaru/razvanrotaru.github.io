322f063cace7c166b1a522e88e8c0d92b60389cf
Razvan Rotaru <razvanrtr@outlook.com>
2021-11-23 01:11:40 +0200

    final layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,6 +50,7 @@ skillsHeader.forEach(el => el.addEventListener('click', toggleSkills))
 
 /*==================== QUALIFICATION TABS ====================*/
 const qualificationTabs = document.querySelectorAll('[data-target]')
+const portfolioContents = Array.from(document.querySelectorAll('.portfolio__content'))
 
 qualificationTabs.forEach(tab => {
     const target = document.querySelector(tab.dataset.target)
@@ -61,9 +62,20 @@ qualificationTabs.forEach(tab => {
     }
 
     tab.addEventListener('click', () => {
-        console.log(target)
-        console.log(target.style)
+        // console.log(target)
+        // console.log(target.style)
 
+        // console.log(tab)
+        // console.log(tab.style)
+
+        if (tab.classList.contains('portfolio__content')) {
+            portfolioContents.filter(content => content != tab).forEach(n => {
+                n.classList.remove('qualification__active')
+                const target = document.querySelector(n.dataset.target)
+                target.classList.remove('qualification__active')
+                target.style.maxHeight = null;
+            })
+        }
 
         if (target.classList.contains('qualification__active')){
             target.style.maxHeight = null;
@@ -81,34 +93,94 @@ qualificationTabs.forEach(tab => {
 /*==================== PORTFOLIO SWIPER  ====================*/
 window.addEventListener('scroll', function() {
 	var element = document.querySelector('.portfolio__grid');
+
+    if (element.classList.contains('portfolio__active')) {
+        return;
+    }
+
 	var position = element.getBoundingClientRect();
-
-    var diff = position.bottom - this.window.innerHeight;
-    console.log('diff: ' + diff)
-
-    element.style.transform = 'rotateY(' + diff / 6 + 'deg)'
+    var thresh = 300;
 
 	// checking whether fully visible
-	if(position.top >= 0 && position.bottom <= window.innerHeight) {
+	if(position.top >= thresh && position.bottom + thresh <= window.innerHeight) {
 		// console.log('Element is fully visible in screen');
+        element.style.transform = null
 	}
 
 	// checking for partial visibility
-	if(position.top < window.innerHeight && position.bottom >= 0) {
+	if(position.top + thresh < window.innerHeight && position.bottom >= thresh) {
 		// console.log('Element is partially visible in screen');
-	}
+        var diff = position.bottom - this.window.innerHeight;
+        // console.log('diff: ' + diff)
+        element.style.transform = 'rotateY(' + diff / 10 + 'deg)'
+    }
 });
 
-/*==================== TESTIMONIAL ====================*/
-
-
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    if (this.scrollY >= 80) {
+        nav.classList.add('scroll-header')
+    } else {
+        nav.classList.remove('scroll-header')
+    }
+}
 
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/ 
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-up')
+    if (this.scrollY >= 560) {
+        scrollTop.classList.add('show-scroll')
+    } else {
+        scrollTop.classList.remove('show-scroll')
+    }
+}
 
+window.addEventListener('scroll', scrollTop)
 
 /*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
